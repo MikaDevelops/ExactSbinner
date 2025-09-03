40cbedca7a2e00ddaca7c5544ef834bb9c57a374
MikaDevelops <mika.virtala@gmail.com>
2025-09-03 22:16:34 +0300

    data from json file

--- js/text_to_array.js
+++ js/text_to_array.js
@@ -1,6 +1,35 @@
 class TextToArray{
     makeArray(text, type){
-        return text.split("\n");
+        switch(type){
+            case 'csv':
+                return text.split("\n");
+                break;
+            case 'json':
+                return this.#jsonToStringArray(text);
+                break;
+            case 'csv_combined':
+                return ['444', 'csv tables are here'];
+                break;
+            default:
+                throw new Error('type not supported in TextToArray.makeArray');
+        }
+        
+    }
+
+    #jsonToStringArray(text){
+        let dataObject = JSON.parse(text);
+        let linesArray = [];
+        for(let key in dataObject.bins){
+            let lineString = key + ", " + dataObject.brands[dataObject.bins[key].brand] + ", "
+            + dataObject.issuers[dataObject.bins[key].issuer] + ", " 
+            + dataObject.types[dataObject.bins[key].type] + ", " 
+            + dataObject.subtypes[dataObject.bins[key].subtype] + ", " 
+            + dataObject.countries[dataObject.bins[key].country] 
+            + "\n";
+            console.log(lineString);
+            linesArray.push(lineString);
+        }
+        return linesArray;
     }
 
 }
